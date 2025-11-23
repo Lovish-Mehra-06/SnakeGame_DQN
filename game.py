be30e964781dace1d7b8a2552f44a7f4dad0c141
LovishMehra <lovish06mehra@gmail.com>
2025-11-23 10:45:30 +0530

    ADDING SS

--- game.py
+++ game.py
@@ -88,13 +88,13 @@ class SnakeGameAI:
 
         if self.is_collision() or self.frame_iteration > 100 * len(self.snake):
             game_over = True
-            reward = -10
+            reward = -20
             return reward, game_over, self.score
 
         # 4. place new food or just move
         if self.head == self.food:
             self.score += 1
-            reward = 10
+            reward = 20
             self._place_food()
         else:
             self.snake.pop()
